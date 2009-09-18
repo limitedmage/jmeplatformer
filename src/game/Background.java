c713e86534195743cfc69ad67cad098d7bd24f1f
limited mage <limited.mage@gmail.com>
2009-09-18 08:54:42 -0500

    Added tiles

--- src/game/Background.java
+++ src/game/Background.java
@@ -3,31 +3,65 @@ package game;
 import java.io.IOException;
 import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.game.TiledLayer;
 
 public class Background
 {
+
 	private Image image;
+	private TiledLayer layer;
+	private static final short rows = 12, columns = 24;
+	private static final short tileSize = 32;
+	private static final short[][] tileData = {
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1},
+		{3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 1},
+		{3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1},
+		{3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	};
 
 	/**
 	 * Creates new image for background from imagePath
 	 * @param imagePath path of background image
 	 */
-	public Background(String imagePath)
+	public Background()
 	{
 		try
 		{
-			image = Image.createImage(imagePath);
+			image = Image.createImage("/img/backgrounds/Tiles1.bmp");
+			layer = new TiledLayer(columns, rows, image, tileSize, tileSize);
+
+			// fill tile data
+			for (int row = 0; row < rows; row++)
+			{
+				for (int col = 0; col < columns; col++)
+				{
+					layer.setCell(col, row, tileData[row][col]);
+				}
+			}
 		}
 		catch (IOException ex)
-		{}
+		{
+			//unable to load images, quit game
+			System.out.println(ex.getMessage());
+			//System.exit(-1);
+		}
 	}
 
 	/**
 	 * Paints background image
 	 * @param g
+	 *
 	 */
 	public void paint(Graphics g)
 	{
-		g.drawImage(image, 0, 0, 0);
+		layer.paint(g);
 	}
 }
