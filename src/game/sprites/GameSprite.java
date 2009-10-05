080e1570ab0cb42d299892821ac718768c0e8581
limited mage <limited.mage@gmail.com>
2009-10-05 09:53:27 -0500

    Documented GameSprite class.

--- src/game/sprites/GameSprite.java
+++ src/game/sprites/GameSprite.java
@@ -5,6 +5,9 @@ import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.game.Sprite;
 import main.Updateable;
 
+/**
+ * Class that defines the basic sprite in the game.
+ */
 public abstract class GameSprite extends Sprite implements Updateable
 {
 	// screen width and height
@@ -13,6 +16,16 @@ public abstract class GameSprite extends Sprite implements Updateable
 	// frame width and height
 	protected int fWidth, fHeight;
 
+	/**
+	 * Creates a new GameSprite.
+	 * 
+	 * @param imgPath - path of the image the sprite should use
+	 * @param sWidth - screen width
+	 * @param sHeight - screen height
+	 * @param fWidth - frame width
+	 * @param fHeight - frame height
+	 * @throws IOException - if image could not be loaded
+	 */
 	public GameSprite(String imgPath, int sWidth, int sHeight, int fWidth, int fHeight) throws IOException
 	{
 		super(Image.createImage(imgPath), fWidth, fHeight);
