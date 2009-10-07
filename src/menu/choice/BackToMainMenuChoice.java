1e1712255b0b17d44665e0a941467b8968edeccb
limited mage <limited.mage@gmail.com>
2009-10-07 11:58:10 -0500

    Documented game pause classes and methods

--- src/menu/choice/BackToMainMenuChoice.java
+++ src/menu/choice/BackToMainMenuChoice.java
@@ -2,14 +2,25 @@ package menu.choice;
 
 import main.GameMidlet;
 
+/**
+ * A menu choice to return to main menu.
+ */
 public class BackToMainMenuChoice extends MenuChoice
 {
-
+	/**
+	 * Creates new choice.
+	 */
 	public BackToMainMenuChoice()
 	{
 		super("Back to main menu");
 	}
 
+	/**
+	 * Excecutes choice by telling midlet to load
+	 * main menu.
+	 *
+	 * @param midlet - GameMidlet that loads the main menu
+	 */
 	public void execute(GameMidlet midlet)
 	{
 		midlet.startMainMenu();
