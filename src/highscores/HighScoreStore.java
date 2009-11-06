1a269e615a50f4519fb5642d0c337d80b09461ad
limited mage <limited.mage@gmail.com>
2009-11-06 12:56:25 -0600

    HighScoreStore now correctly traverses through records with a RecordEnumeration. HighScoreStore also now has a deleteAll method to delete all scores (clear only removes them from the RecordStore, not the vector)

--- src/highscores/HighScoreStore.java
+++ src/highscores/HighScoreStore.java
@@ -6,7 +6,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.Vector;
-import javax.microedition.rms.RecordComparator;
 import javax.microedition.rms.RecordEnumeration;
 import javax.microedition.rms.RecordStore;
 import javax.microedition.rms.RecordStoreException;
