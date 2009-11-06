33d6f8d2d0ccc9c04d92232ba64c8c4d0e583c1d
limited mage <limited.mage@gmail.com>
2009-11-06 12:52:43 -0600

    HighScoreScreen now has choice to clear scores

--- src/highscores/HighScoreScreen.java
+++ src/highscores/HighScoreScreen.java
@@ -1,10 +1,23 @@
 package highscores;
 
+import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Graphics;
 import main.MainMidlet;
 import main.Screen;
 
-public class HighScoreScreen extends Screen
+/**
+ * Screen to show high scores.
+ * High scores are loades from the constructor
+ * agrument midlet's HighScoreStore
+ *
+ * Implements CommandListener and allows to clear scores
+ */
+public class HighScoreScreen extends Screen implements CommandListener
 {
 	private HighScoreStore scores;
 
@@ -14,6 +27,11 @@ public class HighScoreScreen extends Screen
 		super(midlet);
 
 		this.scores = midlet.getScores();
+		this.setFullScreenMode(false);
+
+		this.addCommand(new Command("Back", Command.BACK, 1));
+		this.addCommand(new Command("Clear Scores", Command.SCREEN, 2));
+		this.setCommandListener(this);
 	}
 
 
@@ -26,7 +44,7 @@ public class HighScoreScreen extends Screen
 			g.setColor(0x00000000);
 			g.fillRect(0, 0, getWidth(), getHeight());
 
-			int x = 0;
+			int x = this.getWidth() / 2;
 			int y = 0;
 
 			int dy = this.getHeight() / len;
@@ -35,7 +53,8 @@ public class HighScoreScreen extends Screen
 
 			for (int i = 0; i < len; i++)
 			{
-				g.drawString(this.scores.elementAt(i), x, y, Graphics.HCENTER);
+				String score = this.scores.elementAt(i);
+				g.drawString(score, x, y, Graphics.HCENTER | Graphics.TOP);
 				y += dy;
 			}
 		}
@@ -46,8 +65,48 @@ public class HighScoreScreen extends Screen
 		// press any key to go back to main menu
 		if ((getKeyStates()) != 0)
 		{
-			midlet.startMainMenu();
+			this.midlet.startMainMenu();
 		}
+	}
+
+	public void commandAction(Command c, Displayable d)
+	{
+		switch (c.getCommandType())
+		{
+			case Command.BACK:
+				// back to menu
+				this.midlet.startMainMenu();
+				return;
+
+			case Command.SCREEN:
+				// clear scores and back to menu
+				Alert a = new Alert("Are you sure?", "All high scores will be deleted", null, AlertType.WARNING);
+				a.setTimeout(Alert.FOREVER);
+				a.addCommand(new Command("Yes", Command.OK, 1));
+				a.addCommand(new Command("No", Command.CANCEL, 2));
+				a.setCommandListener(new CommandListener(){
+					public void commandAction(Command c, Displayable d)
+					{
+						switch (c.getCommandType())
+						{
+							case Command.OK:
+								HighScoreScreen.this.scores.deleteAll();
+								HighScoreScreen.this.midlet.startHighScores();
+								break;
+
+							case Command.CANCEL:
+								HighScoreScreen.this.midlet.startHighScores();
+								break;
+						}
+					}
+				});
+				Display.getDisplay(this.midlet).setCurrent(a, this);
+				break;
+
+			default:
+				break;
+		}
+
 	}
 
 }
