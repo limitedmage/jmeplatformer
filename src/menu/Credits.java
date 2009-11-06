8981b54a88ff3b89ee1e78a77c082ee4ee828094
limited mage <limited.mage@gmail.com>
2009-11-06 13:00:15 -0600

    Credits now draws text better and has a command to go back

--- src/menu/Credits.java
+++ src/menu/Credits.java
@@ -1,5 +1,9 @@
 package menu;
 
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Font;
 import main.MainMidlet;
 import main.Screen;
 import javax.microedition.lcdui.Graphics;
@@ -7,12 +11,12 @@ import javax.microedition.lcdui.Graphics;
 /**
  * A credits screen aknowledging all contributors
  */
-public class Credits extends Screen
+public class Credits extends Screen implements CommandListener
 {
     /**
      * Credits text to be shown
      */
-	private String text;
+	private String[] text;
 
     /**
      * Creates a new Credits screen
@@ -21,8 +25,12 @@ public class Credits extends Screen
 	public Credits(MainMidlet midlet)
 	{
 		super(midlet);
+		this.setFullScreenMode(false);
 
-		text = "Our Game!\n\nProgramming:\nAlex\nCarlos\nJuliana\n\nGraphics:\nLADs del TEC-CEM\n";
+		this.text = new String[] {"Our Game!", "Alex", "Carlos", "Juliana", "", "ITESM CEM", "TC2003", "Fall 2009"};
+
+		this.addCommand(new Command("Back", Command.BACK, 1));
+		this.setCommandListener(this);
 	}
 
     /**
@@ -34,7 +42,7 @@ public class Credits extends Screen
 		// press any key to go back to main menu
 		if ((getKeyStates()) != 0)
 		{
-			midlet.startMainMenu();
+			this.midlet.startMainMenu();
 		}
 	}
 
@@ -47,8 +55,32 @@ public class Credits extends Screen
 		g.setColor(0x00000000);
 		g.fillRect(0, 0, getWidth(), getHeight());
 
-		g.setColor(0x00ff0000);
-		g.drawString(text, 0, 0, 0);
+		int len = this.text.length;
+
+		if (len > 0)
+		{
+			int y = 0;
+			int x = getWidth() / 2;
+
+			Font f = g.getFont();
+
+			int dy = getHeight() / len;
+			g.setColor(0x00ff0000);
+			for (int i = 0; i < len; i++)
+			{
+				g.drawString(this.text[i], x, y, Graphics.HCENTER | Graphics.TOP);
+				y += f.getHeight();
+			}
+		}
+	}
+
+	public void commandAction(Command c, Displayable d)
+	{
+		switch (c.getCommandType())
+		{
+			case Command.BACK:
+				this.midlet.startMainMenu();
+		}
 	}
 
 
